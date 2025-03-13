7cab1387215682e9ab276febc896db44c967da88
erickromo117 <rebyvodka11768@gmail.com>
2025-03-13 15:49:21 -0600

    producto

--- back/src/utils/dotenv.ts
+++ back/src/utils/dotenv.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 
-export const PORT = process.env.PORT!;
+export const PORT = process.env.PORT || "3000";
 export const DB_IP = process.env.DB_IP!;
 export const DB_NAME = process.env.DB_NAME!;
 export const DB_PASS = process.env.DB_PASS!;
